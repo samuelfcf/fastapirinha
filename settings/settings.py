c0ff3bf3cac81611a7817fe7e9ce6d003b3259b1
samuel <samuel.fernandes@vittude.com>
2024-02-10 18:16:14 -0300

    config: change settings class after add services to containers

--- settings/settings.py
+++ settings/settings.py
@@ -1,10 +1,11 @@
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8'
+        env_file_encoding='utf-8'
     )
 
-    DATABASE_URL: str
+    DATABASE_URL: str = "postgresql://app_user:app_password@localhost:5432/app_db"
