d860060238a9e4c20bef8fb206ec4d7c66f3c3ce
samuel <samuel.fernandes@vittude.com>
2024-02-11 15:46:47 -0300

    feat: debit transaction

--- src/app.py
+++ src/app.py
@@ -22,6 +22,7 @@ def process_credit(db: Session, body: TransactionSchemaInput, client: Client):
         db.commit()
     except Exception as e:
         print(f'Cannot update saldo: {e}')
+        return None
 
     try:
         transaction_data = {
@@ -32,15 +33,47 @@ def process_credit(db: Session, body: TransactionSchemaInput, client: Client):
         }
         transaction_respository.create(db, transaction_data)
     except Exception as e:
-        print(f'Cannot create transaction: {e}')
+        print(f'Cannot process credit: {e}')
         client.saldo -= value
         db.commit()
+        raise e
 
     return client
 
 
 def process_debit(db: Session, body: TransactionSchemaInput, client: Client):
-    return 1
+    description = body.descricao
+    value = body.valor
+
+    try:
+        client.saldo -= value
+        if client.saldo < -client.limite:
+            client.saldo += value
+            db.commit()
+            return None
+
+        db.commit()
+    except Exception as e:
+        print(f'Cannot process debit: {e}')
+        client.saldo += value
+        db.commit()
+        return None
+
+    try:
+        transaction_data = {
+            'client_id': client.id,
+            'valor': value,
+            'tipo': 'd',
+            'descricao': description,
+        }
+        transaction_respository.create(db, transaction_data)
+    except Exception as e:
+        print(f'Cannot create transaction: {e}')
+        client.saldo += value
+        db.commit()
+        raise e
+
+    return client
 
 
 def get_client_by_id(db: Session, id: str):
@@ -56,7 +89,7 @@ def process_transaction(
         case 'c':
             updated_client = process_credit(db, body, client)
         case 'd':
-            process_debit(db, body, client)
+            updated_client = process_debit(db, body, client)
 
     return updated_client
 
@@ -73,10 +106,13 @@ def transactions(
 ):
     client = get_client_by_id(db, id)
     if not client:
-        response.status_code = 404
+        response.status_code = status.HTTP_404_NOT_FOUND
         return response
 
     updated_client = process_transaction(db, body, client)
+    if not updated_client:
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return response
 
     response.status_code = status.HTTP_200_OK
     return updated_client
