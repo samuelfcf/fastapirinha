a01a7a914c5f9be76e6375996b8ec022c0553e3a
samuel <samuel.fernandes@vittude.com>
2024-02-11 13:47:52 -0300

    input base object renamed

--- src/schemas.py
+++ src/schemas.py
@@ -2,9 +2,13 @@ from pydantic import BaseModel, PositiveInt, constr
 from typing_extensions import Literal
 
 
-class TransactionSchema(BaseModel):
+class TransactionSchemaInput(BaseModel):
     valor: PositiveInt
     tipo: Literal['c', 'd']   # type: ignore
     descricao: constr(
         strip_whitespace=True, min_length=1, max_length=10
     )   # type: ignore
+
+class TransactionSchemaResponse(BaseModel):
+    limite: int
+    saldo: int
