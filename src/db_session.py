ba2e1c3d16039a56f77895527a983499d0598692
samuel <samuel.fernandes@vittude.com>
2024-02-15 22:51:19 -0300

    feat: first performance improvements

--- src/db_session.py
+++ src/db_session.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    'postgresql://app_user:app_password@api_rinha_db:5432/app_db'
+    'postgresql://app_user:app_password@api_rinha_db:5432/app_db',
+    pool_size=1000,
+    max_overflow=1000
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- src/repository.py
+++ src/repository.py
@@ -8,47 +8,66 @@ from src.models import Client, Transaction
 
 class ClientRepository:
     def get_by_id(self, db: Session, id: int) -> Client:
-        return db.query(Client).get(id)
+        try:
+            return db.query(Client).with_for_update().get(id)
+        except Exception as e:
+            print(f'Error [get_by_id]: {e}')
+        finally:
+            db.close()
 
     def create(self, db: Session, obj) -> Client:
+        try:
+            client = Client(
+                created_at=datetime.now(),
+                **obj,
+            )
 
-        client = Client(
-            created_at=datetime.now(),
-            **obj,
-        )
-
-        db.add(client)
-        db.commit()
-        db.refresh(client)
+            db.add(client)
+            db.commit()
+            db.refresh(client)
+        except Exception as e:
+            print(f'Error [create client]: {e}')
+        finally:
+            db.close()
 
         return client
 
 
 class TransactionRepository:
     def create(self, db: Session, obj) -> Transaction:
+        try:
+            transaction = Transaction(
+                realizada_em=datetime.now(),
+                **obj,
+            )
 
-        transaction = Transaction(
-            realizada_em=datetime.now(),
-            **obj,
-        )
-
-        db.add(transaction)
-        db.commit()
-        db.refresh(transaction)
+            db.add(transaction)
+            db.commit()
+            db.refresh(transaction)
+        except Exception as e:
+            print(f'Error [create transaction]: {e}')
+        finally:
+            db.close()
 
         return transaction
 
     def get_last_transactions(
         self, db: Session, client_id: int, limit: int = 10
     ) -> list[Transaction]:
-        transactions = (
-            db.query(Transaction)
-            .filter(Transaction.client_id == client_id)
-            .order_by(desc(Transaction.realizada_em))
-            .limit(limit)
-            .all()
-        )
-        if not transactions:
-            return None
+        try:
+            transactions = (
+                db.query(Transaction)
+                .filter(Transaction.client_id == client_id)
+                .with_for_update()
+                .order_by(desc(Transaction.realizada_em))
+                .limit(limit)
+                .all()
+            )
+            if not transactions:
+                return None
+        except Exception as e:
+            print(f'Error [get_last_transaction]: {e}')
+        finally:
+            db.close()
 
         return transactions
